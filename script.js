3ad2992e337011357ecab9a5e4ee6542bae1a8cc
Milena Bertolini <milenaarantesb@gmail.com>
2023-07-06 17:37:17 -0300

    funções converter decimal

---
+++ script.js
@@ -0,0 +1,7 @@
+function decimalParaBinario(decimal){
+    return parseInt(decimal).toString(2);
+}
+
+function decimalParaHexadecimal(decimal) {
+    return parseInt(decimal).toString(16).toUpperCase();
+}
